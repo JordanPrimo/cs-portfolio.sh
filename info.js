3be649e158b691ce9b59f955e440f72a16c11415
jordanprimo <jprimo.sh@mysummitps.org>
2016-01-14 23:08:22 +0000

    update

--- info.js
+++ info.js
@@ -7,21 +7,21 @@ var info2 = document.getElementById("info2");
 var info3 = document.getElementById("info3");
 
 item1.addEventListener("click", function(){
-    info1.innerHTML = "Super smash bros brawl, is a really fun game to play with family, that is played on the wii.";
-    info2.innerHTML = "In super smash bros you can play as many characters such as mario, donkey kong, link, and many more.";
-    info3.innerHTML = "some other stufffff"
+    info1.innerHTML = "1. Super smash bros brawl, is a really fun game to play with family, that is played on the wii.";
+    info2.innerHTML = "2. In super smash bros you can play as many characters such as mario, donkey kong, link, and many more.";
+    info3.innerHTML = "3. The first weekend super smash bros was out it sold 490,000 copies!"
 });
                        
 item2.addEventListener("click", function(){
-    info1.innerHTML = "Item 2 stuff";
-    info2.innerHTML = "Item 2 other stuff";
-    info3.innerHTML = "item 2 other stuffff";
+    info1.innerHTML = "1. Fifa is a really fun soccer game, that has been coming out every year since 1993.";
+    info2.innerHTML = "2. In fifa you can play as many different soccer teams from different leagues, create your own career, and build your own personal team.";
+    info3.innerHTML = "3. ";
 });
 
 item3.addEventListener("click", function(){
-    info1.innerHTML = "Item 3 stuf";
-    info2.innerHTML = "item 3 stuff";
-    info3.innerHTML = "item 3 stufff";
+    info1.innerHTML = "1. Counter strike is a really fun first-person shooter, that has had many different versions of the game.";
+    info2.innerHTML = "2. In Counter Strike Global Offensive you can collect skins, knives, play in a deathmatch, and play in a competative match making.";
+    info3.innerHTML = "3. Counter strike is played proffesionally, and there are tournaments in which the winning team can win thousands of dollars.";
 });
 
 item4.addEventListener("click", function(){
